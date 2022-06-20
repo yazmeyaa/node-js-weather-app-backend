013a753f1a9a47bc027dfbf03deb62bf26d5c08e
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-06-20 22:55:14 +0300

    Go another one

--- routes/api/getWeatherByIp.ts
+++ routes/api/getWeatherByIp.ts
@@ -23,7 +23,7 @@ export async function getWeatherByIP(req: Request, res: Response) {
         }
     })
     .then( data => {
-        return res.status(200).send(data.data)
+        return res.status(200).send({message: `your IP is ${clientIPaddress}`})
     })
     .catch( error => {
         return res.status(200).send(error)
