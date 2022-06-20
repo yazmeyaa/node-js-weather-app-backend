4a58b3b607e7b77ad3f2ff001299feb0017c1883
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-06-20 22:25:41 +0300

    Please work T_T

--- routes/api/getWeatherByIp.ts
+++ routes/api/getWeatherByIp.ts
@@ -6,10 +6,12 @@ export async function getWeatherByIP(req: Request, res: Response) {
     const secretAPIkey = process.env.WEATHER_API_KEY
     const clientIPaddress = req.ip.split(':')[3]
 
+    if (!clientIPaddress) {
+        return res.status(400).send({ error: 'Where is IP?' })
+    }
     if (!secretAPIkey) {
         throw new Error('WEATHER_API_KEY IS REQUIRED')
     }
-
     axios({
         method: 'GET',
         url: 'http://api.weatherapi.com/v1/current.json',
